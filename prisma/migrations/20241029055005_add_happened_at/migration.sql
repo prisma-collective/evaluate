6fc915c87db419d9152a6201c3e2b1434d035844
Tobias Fechner <tobias.fechner@hotmail.co.uk>
2025-06-27 17:55:32 +0200

    Purge prisma orm

--- prisma/migrations/20241029055005_add_happened_at/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - Added the required column `happenedAt` to the `TimelineEntry` table without a default value. This is not possible if the table is not empty.
-
-*/
--- AlterTable
-ALTER TABLE "TimelineEntry" ADD COLUMN     "happenedAt" TIMESTAMP(3) NOT NULL;
--- src/lib/db.js
+++
@@ -1,6 +0,0 @@
-// src/lib/db.js
-import { PrismaClient } from '@prisma/client';
-
-const prisma = new PrismaClient();
-
-export default prisma;
